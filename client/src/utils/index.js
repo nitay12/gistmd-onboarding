d85c56499a676ddfdafdccba730b539485b9b4fb
Nitay Caspi <NitayCaspi12@gmail.com>
2021-12-20 10:43:52 +0200

    check if age is under 1 year

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,11 @@
 export const ageCalculator = (date) => {
   const birthDate = new Date(date);
   const today = new Date();
-  const age = today.getFullYear() - birthDate.getFullYear();
-  return age;
+  const ageByYear = today.getFullYear() - birthDate.getFullYear();
+  const ageByMonth = today.getMonth() + 1 - birthDate.getMonth() + 1;
+  if (ageByYear > 0) return ageByYear;
+  else {
+    return ageByMonth + " mo."
+  }
+  //TODO: check for days?
 };
